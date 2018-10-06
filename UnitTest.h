642cc8244c31b403292f28ee4b55507171a7b569
wzh <2663976081@qq.com>
2018-10-06 20:46:59 +0800

    code

---
+++ UnitTest.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "ColorConsole.h"
+#define UT_EXPECT_TRUE(expr) \
+{ \
+auto value = bool(expr); \    
+if(!value) \
+{ \
+    std::cout <<CODE_LOCATION << ":";\
+    std::cout << #expr " is expected to "; \
+    CConsole(GREEN)<<"true, but it is "; \
+    CConsole(RED) << "false\n" \
+} \
+}
+
+#define UT_EXPECT_FALSE(expr) \
+{ \
+auto value = bool(expr); \    
+if(value) \
+{ \
+    std::cout <<CODE_LOCATION << ":";\
+    std::cout << #expr " is expected to "; \
+    CConsole(GREEN)<<"false, but it is "; \
+    CConsole(RED) << "true\n" \
+} \
+}
